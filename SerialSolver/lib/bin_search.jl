1876fb436d49697b08d7d2d65a03322ca50773af
Draco112358 <gianni.frittella@gmail.com>
2023-06-21 12:09:05 +0200

    added bin_search file

---
+++ SerialSolver/lib/bin_search.jl
@@ -0,0 +1,20 @@
+function bin_search(num, A)
+    index = 0
+    n = length(A)
+    left = 1
+    right = n
+    while left <= right
+        mid = ceil((left + right) / 2)
+        if A[mid] == num
+            index = mid
+            break
+        else
+            if A[mid] > num
+                right = mid - 1
+            else
+                left = mid + 1
+            end
+        end
+    end
+    return index
+end
