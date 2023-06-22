9bd9a23defa3abb654123538a97f3542b5dca1c4
edgardovittoria <edgardovittoria@hotmail.it>
2023-06-22 11:31:42 +0200

    porting of create_volumes_mapping_v2.mat

---
+++ SerialSolver/lib/create_volumes_mapping_v2.jl
@@ -0,0 +1,20 @@
+function create_volumes_mapping_v2(grids)
+    num_grids = length(grids)
+    Nx, Ny, Nz = size(grids[1])
+    mapping = zeros(Nx*Ny*Nz)
+    num_ele = 0
+    for cont = 1:Nx
+        for cont2 = 1:Ny
+            for cont3 = 1:Nz
+                for k = 1:num_grids
+                    if grids[k][cont, cont2, cont3] != 0
+                        num_ele += 1
+                        mapping[From_3D_to_1D(cont, cont2, cont3, Nx, Ny)] = num_ele
+                        break
+                    end
+                end
+            end
+        end
+    end
+    return mapping, num_ele
+end
